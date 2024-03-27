e6984e46ac83a1451fc052520d6f098b1ecbeedb
Marco Dias <marcodias.bko@gmail.com>
2024-03-27 00:02:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import "./Global.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="tree">
+          <ul>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+                <div className="box">Person</div>
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+              </div>
+            </li>
+            <li>
+              <div className="group">
+                <div className="box">Person</div>
+                <div className="box">Person</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
