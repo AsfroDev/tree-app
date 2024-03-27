cd96ed3b43ce65c40943d64fddc5e8f3c7c163bd
AsfroDev <marcodias.bko@gmail.com>
2024-03-27 10:31:32 -0300

    modal update

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,211 @@
 import "./Global.css";
 import { Modal } from "./Modal";
+import { useState } from "react";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [modalPosition, setModalPosition] = useState({ x: 0, y: 0 });
 
-  
+  const updateModalPosition = (e) => {
+    setModalPosition({ x: e.clientX, y: e.clientY });
+  };
 
   return (
     <>
+      {showModal && (
+        <div
+          className="modal"
+          style={{ left: modalPosition.x, top: modalPosition.y, zIndex: 2, }}
+        >
+          <Modal />
+        </div>
+      )}
+
       <div className="container">
         <div className="tree">
           <ul>
             <li>
               <div className="group">
-                <div className="box">Person</div>
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
-                <div className="box">Person</div>
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
             <li>
               <div className="group">
-                <div className="box">Person</div>
-                <div className="box">Person</div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
+                <div
+                  className="box"
+                  onMouseEnter={() => setShowModal(true)}
+                  onMouseLeave={() => setShowModal(false)}
+                  onMouseMove={updateModalPosition}
+                >
+                  Person
+                </div>
               </div>
             </li>
           </ul>
